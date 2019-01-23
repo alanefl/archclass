9b426e032d841b68ebbc09d0ec60a626974e1e11
alanefl <alanf94@gmail.com>
2019-01-23 09:31:21 -0800

    base_model -> test_model, and updated multinomial log regression for model and params

---
+++ constants.py
@@ -0,0 +1,154 @@
+
+ARCHITECTURE_STYLES = [
+  {
+    "name": "achaemenid",
+    "id": 0,
+    "start_year": -550,
+    "end_year": -220
+  },
+  {
+    "name": "american_foursquare",
+    "id": 1,
+    "start_year": 1890,
+    "end_year": 1930
+  },
+  {
+    "name": "american_craftsman",
+    "id": 2,
+    "start_year": 1860,
+    "end_year": 1930
+  },
+  {
+    "name": "ancient_egyptian",
+    "id": 3,
+    "start_year": -2580,
+    "end_year": -2400
+  },
+  {
+    "name": "art_deco",
+    "id": 4,
+    "start_year": 1930,
+    "end_year": 2000
+  },
+  {
+    "name": "art_noveau",
+    "id": 5,
+    "start_year": 1895,
+    "end_year": -1915
+  },
+  {
+    "name": "baroque",
+    "id": 6,
+    "start_year": 1600,
+    "end_year": 1700
+  },
+  {
+    "name": "bauhaus",
+    "id": 7,
+    "start_year": 1919,
+    "end_year": 1933
+  },
+  {
+    "name": "beaux_arts",
+    "id": 8,
+    "start_year": 1671,
+    "end_year": 1968
+  },
+  {
+    "name": "byzantine",
+    "id": 9,
+    "start_year": 600,
+    "end_year": 800
+  },
+  {
+    "name": "chicago_school",
+    "id": 10,
+    "start_year": 1900,
+    "end_year": 1970
+  },
+  {
+    "name": "colonial",
+    "id": 11,
+    "start_year": 1600,
+    "end_year": 1900
+  },
+  {
+    "name": "deconstructivism",
+    "id": 12,
+    "start_year": 1980,
+    "end_year": 2000
+  },
+  {
+    "name": "edwardian",
+    "id": 13,
+    "start_year": 1901,
+    "end_year": 1914
+  },
+  {
+    "name": "georgian",
+    "id": 14,
+    "start_year": 1720,
+    "end_year": 1840
+  },
+  {
+    "name": "gothic",
+    "id": 15,
+    "start_year": 1200,
+    "end_year": 1600
+  },
+  {
+    "name": "greek_revival",
+    "id": 16,
+    "start_year": 1800,
+    "end_year": 1900
+  },
+  {
+    "name": "international",
+    "id": 17,
+    "start_year": 1920,
+    "end_year": 1930
+  },
+  {
+    "name": "novelty",
+    "id": 18,
+    "start_year": 1900,
+    "end_year": 2000
+  },
+  {
+    "name": "palladian",
+    "id": 19,
+    "start_year": 1500,
+    "end_year": 1900
+  },
+  {
+    "name": "postmodern",
+    "id": 20,
+    "start_year": 1970,
+    "end_year": 2000
+  },
+  {
+    "name": "queen_anne",
+    "id": 21,
+    "start_year": 1800,
+    "end_year": 1910
+  },
+  {
+    "name": "romanesque",
+    "id": 22,
+    "start_year": 800,
+    "end_year": 1200
+  },
+  {
+    "name": "russian_revival",
+    "id": 23,
+    "start_year": 1825,
+    "end_year": 1917
+  },
+  {
+    "name": "tudor_revival",
+    "id": 24,
+    "start_year": 1900,
+    "end_year": 1920
+  }
+]
+
